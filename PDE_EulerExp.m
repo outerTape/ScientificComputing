065ab264a76ab7ce7cd78fc924788c880f64769d
jonat <jonat@DESKTOP-DGM4AHM.fritz.box>
2020-03-21 01:17:04 +0900

    Added a a template for the Euler method.

---
+++ PDE_EulerExp.m
@@ -0,0 +1,13 @@
+function [outputArg1,outputArg2] = PDE_EulerExp(fun,u0,t0,n)
+% Input arguments:  
+%      fun  the name of the right-hand-side EDO function
+%      t0   the initial time
+%      u0   the initial condition at t0
+%      t1   the final time
+%      n    the number of time steps between t0 and t1
+% Outputs arguments:
+%      u    the dimension (n+1) vector containing the numerical
+%               solution at times t0+i*h, with h=(t1-t0)/n
+
+end
+
