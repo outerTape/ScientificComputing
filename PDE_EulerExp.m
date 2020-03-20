bfc3a0c64de48db2a31de3efae60629ada31c884
jonat <jonat@DESKTOP-DGM4AHM.fritz.box>
2020-03-21 02:21:59 +0900

    Finished Euler method.

--- PDE_EulerExp.m
+++ PDE_EulerExp.m
@@ -1,4 +1,4 @@
-function [outputArg1,outputArg2] = PDE_EulerExp(fun,u0,t0,n)
+function u = PDE_EulerExp(fun, u0, t0, n)
 % Input arguments:  
 %      fun  the name of the right-hand-side EDO function
 %      t0   the initial time
@@ -9,6 +9,11 @@ function [outputArg1,outputArg2] = PDE_EulerExp(fun,u0,t0,n)
 %      u    the dimension (n+1) vector containing the numerical
 %               solution at times t0+i*h, with h=(t1-t0)/n
 
-
+    t = linspace(t0, t1, n + 1);
+    u = zeros(1, n + 1);
+    u(1) = u0;
+    for i=1:n
+        u(i+1) = u(i) + h*fun(t(i), u(i));
+    end
 end
 
